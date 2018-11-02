d37bb232b93cc2fdcebb3bbb20db7282b1649a8b
Priyanka2095 <priyanka.saha@memeinfotech.com>
2018-11-02 12:25:36 +0530

    dashboard ui

--- src/app/newlogin/newlogin.component.ts
+++ src/app/newlogin/newlogin.component.ts
@@ -7,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewloginComponent implements OnInit {
   public myAngularxQrCode: string = null;
+  public loginShow: boolean = true;
+  public otpShow: boolean = false;
   constructor() {
     this.myAngularxQrCode = 'Your QR code data string';
    }
 
   ngOnInit() {
+  }
+
+  login() {
+  this.otpShow=true;
+  this.loginShow=false;  
   }
 
 }
