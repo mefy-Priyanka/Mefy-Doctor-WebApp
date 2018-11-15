7b28168ac0d90edd4b47a303155fce55a9d73d0c
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-11-15 11:30:57 +0530

    dashboard navigation problem solved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {HttpModule} from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { TextMaskModule } from 'angular2-text-mask';
@@ -78,6 +79,7 @@ const approutes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    HttpModule,
     QRCodeModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(approutes),   //,{useHash: true}
@@ -102,7 +104,8 @@ const approutes: Routes = [
     AccountService,
     BillService,
     DocregistrationService,
-    LoginService
+    LoginService,
+    HttpClientModule
     // ManageaccountComponent,
     // AuthGuard
   ],
