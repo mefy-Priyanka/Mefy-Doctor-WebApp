38606a4464feed152c7042aa79bd557511c8cc82
meme-neha <neha.keshri@memeinfotech.com>
2018-11-15 13:03:37 +0530

    calender added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TextMaskModule } from 'angular2-text-mask';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TagInputModule } from 'ngx-chips';
 // import { ToastrModule } from 'ngx-toastr';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 /********************NEW SERVICES************************** */
 import { DocregistrationService } from './mefyservice/docregistration.service';
 import { LoginService } from './mefyservice/login.service';
@@ -83,6 +84,7 @@ const approutes: Routes = [
     RouterModule.forRoot(approutes),   //,{useHash: true}
     TextMaskModule,
     TagInputModule,
+    BsDatepickerModule.forRoot()
     // ToastrModule.forRoot({   // ToastrModule added
     //   timeOut: 999,
     //   preventDuplicates: true,
