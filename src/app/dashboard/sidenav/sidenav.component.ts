06e3190a257ca6e782134fe483b9a5ee9b1ac17c
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-12-05 12:45:09 +0530

    doctor availability issue fixed

--- src/app/dashboard/sidenav/sidenav.component.ts
+++ src/app/dashboard/sidenav/sidenav.component.ts
@@ -1,41 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProfileService } from '../../mefyservice/profile.service';
+import { SharedService } from '../../mefyservice/shared.service';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
+
 export class SidenavComponent implements OnInit {
+  /************************************ USED VARIABLES ******************************************** */
   doctorProfileId: any;
   profileInfo: any;
   status: any;
   loginStatus: boolean = false;
-  constructor( private router: Router,private profileService:ProfileService) {
-    this.doctorProfileId = localStorage.getItem('doctorId');
+  /***********************************************************************************************/
+
+  constructor(private router: Router, private profileService: ProfileService, private sharedService: SharedService) {
+
+    this.doctorProfileId = localStorage.getItem('doctorId');  //GET DOCTORID FROM LOCALSTORAGE
+
   }
 
   ngOnInit() {
-    this.doctorProfile();
+    this.doctorProfile();     //GET DOCTOR PROFILE DETAILS
   }
 
-  //get doctorBasic Info
+  /***************************************** GET DOCTOR PROFILE DEATILS *************************************** */
   doctorProfile() {
-    console.log("doctorId",this.doctorProfileId);
-    this.profileService.getDocDetail(this.doctorProfileId).subscribe(data=>{
-      console.log('result', data);
+    console.log("DOCTORID", this.doctorProfileId);
+    this.profileService.getDocDetail(this.doctorProfileId).subscribe(data => {
+      console.log('PROFILE DETAILS', data);
       this.profileInfo = data;
-      console.log("details",this.profileInfo);
-
-console.log('doctorid');
-
+      this.status = this.profileInfo.availability ? this.profileInfo.availability : 'Online';
+      // console.log("PROFILE DETAILS", this.profileInfo);
+    }, err => {
     })
   }
-  
-  // update availaible status of doctor
-  availabilityDoctor(event) {
+  /********************************************** ENDS ********************************************** */
 
+  /******************************* UPDATE STATUS OF AVAILABILITY OF DOCTOR AS ONLINE OR OFFLINE ************************ */
+  availabilityDoctor(event) {
+    console.log('inside availabilty')
     if (event.target.checked) {
       this.status = 'Online';
     }
@@ -44,24 +51,51 @@ console.log('doctorid');
     }
     let available = {
       availability: this.status,
-      userId:  localStorage.getItem('doctorId')
+      userId: localStorage.getItem('userId')
     }
+    // API CALL FOR CHANGING STATUS
     this.profileService.doctorAvailability(available).subscribe(data => {
-      let response:any={};
-      response=data;
-      this.loginStatus = true;
-      console.log('availability', data);
-      this.status = response.result.availability;
-      console.log(this.status);
+      let response: any = {};
+      response = data;
+      if (!response.result.error) {
+        // this.loginStatus = true;
+        console.log('availability', response);
+        this.status = response.result.result.availability;
+        console.log(this.status);
+        let notifydata = {
+          type: 'success',
+          title: 'Availability',
+          msg: 'Updated Successfully'
+        }
+        this.sharedService.createNotification(notifydata);
+      }
+      else {
+        let notifydata = {
+          type: 'error',
+          title: 'Availability',
+          msg: 'Not Updated'
+        }
+        this.sharedService.createNotification(notifydata);
+      }
+
     },
       err => {
+        let notifydata = {
+          type: 'error',
+          title: 'Availability',
+          msg: 'Not Updated'
+        }
+        this.sharedService.createNotification(notifydata);
       })
   }
-  // logout from webapp
+
+  /*********************************************** ENDS ****************************************************** */
+
+  /***************************************DOCTOR LOGOUT FROM APP ******************************************** */
   logOut() {
     localStorage.clear();
     this.router.navigate(['/newlogin']);
   }
-
+  /**************************************** ENDS *************************************************************** */
 
 }
