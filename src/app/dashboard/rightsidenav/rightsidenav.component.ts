4a0821677932befaff6a904541352426701731f7
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-12-13 10:43:23 +0530

    appointment status change

--- src/app/dashboard/rightsidenav/rightsidenav.component.ts
+++ src/app/dashboard/rightsidenav/rightsidenav.component.ts
@@ -20,19 +20,19 @@ import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browse
 export class RightsidenavComponent implements OnInit {
 
 
-  public ActiveAppointment:any=[];
-  public informationNew:any=[];
+  public ActiveAppointment: any = [];
+  public informationNew: any = [];
   public selectedAppointmentData: any = {};
-  public cancelAppointmentId:any={};
-  public noAppointment:boolean=false;  /* Show and hide appointment message*/
-  public loader:boolean=true;
-  public appointmentData:boolean=true;
+  public cancelAppointmentId: any = {};
+  public noAppointment: boolean = false;  /* Show and hide appointment message*/
+  public loader: boolean = true;
+  public appointmentData: boolean = true;
   public elementRef;
-  public appointmentDate:any;
-  public selectedAppointmentdate:any;
-  public getAppointment:any;
-  public Date:any
-  public myDateYMD= new Date();
+  public appointmentDate: any;
+  public selectedAppointmentdate: any;
+  public getAppointment: any;
+  public Date: any
+  public myDateYMD = new Date();
 
 
 
@@ -43,61 +43,61 @@ export class RightsidenavComponent implements OnInit {
     private sharedService: SharedService, myElement: ElementRef, private sanitizer: DomSanitizer) {
     this.elementRef = myElement;
     /*get Appointment List*/
-  this.sharedService.appointmentList.subscribe(data=>{
-  let result:any={}
-  result=data
-  if (Object.keys(data).length != 0) {
-  this.ActiveAppointment.push(result.result)
-  this.getCurrentDateAppointmentlist();
-  }
-})
+    this.sharedService.appointmentList.subscribe(data => {
+      let result: any = {}
+      result = data
+      if (Object.keys(data).length != 0) {
+        this.ActiveAppointment.push(result.result)
+        this.getCurrentDateAppointmentlist();
+      }
+    })
   }
 
   ngOnInit() {
     this.getCurrentDateAppointmentlist();
   }
-/*****************GET CURRENT DATE DOCTOR"S APPOINTMENT ACTIVE LIST***************** */
+  /*****************GET CURRENT DATE DOCTOR"S APPOINTMENT ACTIVE LIST***************** */
   getCurrentDateAppointmentlist() {
-    this.appointmentService.getDoctorCurrentAppointment(localStorage.getItem('doctorId')).subscribe(data=>{
-      this.loader=false;
-      let result:any={}
-      result=data
-      if(result.result.result != null && Object.keys(result.result.result).length != 0){
+    this.appointmentService.getDoctorCurrentAppointment(localStorage.getItem('doctorId')).subscribe(data => {
+      this.loader = false;
+      let result: any = {}
+      result = data
+      if (result.result.result != null && Object.keys(result.result.result).length != 0) {
 
-      var status = result.result.result.filter(function(status) {
-        return status.status =='Active' ;
-      });
-      for (let i = 0; i < status.length; i++) {
-        if( status!= null && Object.keys(status).length != 0){
-        console.log('Active Appointment list',status)
-        this.ActiveAppointment=status
-        this.appointmentData=true;
-        this.noAppointment=false
-        this.loader=false;
+        var status = result.result.result.filter(function (status) {
+          return status.status == 'Active';
+        });
+        for (let i = 0; i < status.length; i++) {
+          if (status != null && Object.keys(status).length != 0) {
+            console.log('Active Appointment list', status)
+            this.ActiveAppointment = status
+            this.appointmentData = true;
+            this.noAppointment = false
+            this.loader = false;
+          }
+
+          else {
+            this.loader = false;
+            this.appointmentData = false;
+            this.noAppointment = true;
+          }
+
+        }
       }
-
-     else {
-      this.loader=false;
-        this.appointmentData=false;
-        this.noAppointment=true;
-      } 
-
-    }
-  }
-  else{
-    this.appointmentData=false;
-        this.noAppointment=true;
-        this.loader=false;
-  }
-    },err=>{
-      this.loader=false;
+      else {
+        this.appointmentData = false;
+        this.noAppointment = true;
+        this.loader = false;
+      }
+    }, err => {
+      this.loader = false;
       console.log(err)
       let notification = {
         type: 'warning',
         title: 'Server Issue ',
       }
       this.sharedService.createNotification(notification);
-    }) 
+    })
   }
 
   // get type to filter data
@@ -116,53 +116,53 @@ export class RightsidenavComponent implements OnInit {
     // }
   }
 
- /**************** API FOR DOCTOR"S APPOINTMENT LIST BY DATE AND DOCTORID **************/
+  /**************** API FOR DOCTOR"S APPOINTMENT LIST BY DATE AND DOCTORID **************/
   selectedDate(date) {
-    console.log('date',date)
-    this.loader=true;
+    console.log('date', date)
+    this.loader = true;
     // this.message = '';
     // this.hideConsult = false;
     if (date != null) {
       this.Date = date;
-    this.appointmentDate = date;
-    this.selectedAppointmentdate = moment(this.appointmentDate).format('YYYY-MM-DD');
-    console.log('selectedAppointmentdate',this.selectedAppointmentdate)
+      this.appointmentDate = date;
+      this.selectedAppointmentdate = moment(this.appointmentDate).format('YYYY-MM-DD');
+      console.log('selectedAppointmentdate', this.selectedAppointmentdate)
       this.appointmentService.getAppointmentList(localStorage.getItem('doctorId'), this.selectedAppointmentdate).subscribe(data => {
-          console.log(data);
-          this.loader=false;
-          let result:any={}
-          result=data
-          if(result.result.result != null && Object.keys(result.result.result).length != 0){
-          var status = result.result.result.filter(function(status) {
-            return status.status =='Active' ;
+        console.log(data);
+        this.loader = false;
+        let result: any = {}
+        result = data
+        if (result.result.result != null && Object.keys(result.result.result).length != 0) {
+          var status = result.result.result.filter(function (status) {
+            return status.status == 'Active';
           });
           for (let i = 0; i < status.length; i++) {
-            if( status!= null && Object.keys(status).length != 0){
-            console.log('Active Appointment list',status)
-            this.ActiveAppointment=status
-            this.appointmentData=true;
-            this.noAppointment=false;
-            this.loader=false;
+            if (status != null && Object.keys(status).length != 0) {
+              console.log('Active Appointment list', status)
+              this.ActiveAppointment = status
+              this.appointmentData = true;
+              this.noAppointment = false;
+              this.loader = false;
+            }
+
+            else {
+              this.appointmentData = false;
+              this.noAppointment = true;
+              this.loader = false;
+              // let notification = {
+              //   type: 'warning',
+              //   title: 'No any Appointment ',
+              // }            
+              // this.sharedService.createNotification(notification);
+            }
+
           }
-
-         else {
-            this.appointmentData=false;
-            this.noAppointment=true;
-            this.loader=false;
-            // let notification = {
-            //   type: 'warning',
-            //   title: 'No any Appointment ',
-            // }            
-            // this.sharedService.createNotification(notification);
-          } 
-
         }
-      }
-      else{
-        this.appointmentData=false;
-            this.noAppointment=true;
-            this.loader=false;
-      }
+        else {
+          this.appointmentData = false;
+          this.noAppointment = true;
+          this.loader = false;
+        }
       },
         err => {
           console.log(err)
@@ -174,7 +174,7 @@ export class RightsidenavComponent implements OnInit {
         });
     }
   }
-/*****************GET APPOINTMENT ID FROM CANCEL BUTTON *****************/
+  /*****************GET APPOINTMENT ID FROM CANCEL BUTTON *****************/
   cancelAppointmentButton(data) {
     this.selectedAppointmentData = data;
     this.cancelAppointmentId = this.selectedAppointmentData.appointmentId;
@@ -183,20 +183,35 @@ export class RightsidenavComponent implements OnInit {
 
   /************************************Cancel Appointment******************/
   cancelAppointment() {
-    this.loader=true;
-// console.log('appointment',this.cancelAppointmentId)
-let data={
-  appointmentId:this.cancelAppointmentId
-}
-this.appointmentService.cancelAppointment(data).subscribe(data=>{
-  console.log('cancelAppointment',data)
-  this.loader=false;
-  let result:any={}
-  result=data
-  this.sharedService.cancdelAppointmentData(result.result.result)
-  this.getCurrentDateAppointmentlist()
+    this.loader = true;
+    // console.log('appointment',this.cancelAppointmentId)
+    let data = {
+      appointmentId: this.cancelAppointmentId,
+      status: 'Cancelled'
+    }
+    this.appointmentService.changeAppointmentStatus(data).subscribe(data => {
+      console.log('cancelAppointment', data)
+      this.loader = false;
+      let result: any = {}
+      result = data;
+      
+      this.sharedService.cancdelAppointmentData(result.result.result)
+      this.getCurrentDateAppointmentlist();
+      let notification = {
+        type: 'success',
+        title: 'Appointment ',
+        msg:'Cancelled Sucessfully'
+      }
+      this.sharedService.createNotification(notification);
 
-})
+    },err=>{
+      let notification = {
+        type: 'error',
+        title: 'Appointment ',
+        msg:'Cancellelation Failed'
+      }
+      this.sharedService.createNotification(notification);
+    })
   }
 
   // Detecting Clicks Outside the Component
