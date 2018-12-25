c6d09962777a3657c43ae3831309cd16ec365e37
meme-neha <neha.keshri@memeinfotech.com>
2018-12-25 14:51:18 +0530

    small change

--- src/app/dashboard/navbar/navbar.component.ts
+++ src/app/dashboard/navbar/navbar.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit,ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { RouterLink, ActivatedRoute, Router } from '@angular/router';
 import { DoctorPrescriptionService } from '../../meme-services/doctor-prescription.service';
-import{SharedService} from '../../mefyservice/shared.service'
-import{ClinicService} from '../../mefyservice/clinic.service'
+import { SharedService } from '../../mefyservice/shared.service'
+import { ClinicService } from '../../mefyservice/clinic.service'
 
 
 @Component({
@@ -14,45 +14,60 @@ import{ClinicService} from '../../mefyservice/clinic.service'
   }
 })
 export class NavbarComponent implements OnInit {
-  doctorProfileId: any;
-  prescriptionId:any;
-  whenDashboardClicked :Boolean= true;  /*for Dashboard*/
-  whenCreditClicked:Boolean=false;      /*for Credit*/
-  whenAppoinmentClicked:Boolean=false;  /*for Appointment*/
-  whenClinicsClicked:Boolean=false;     /*for Clinic*/
-  whenEConsultClicked:Boolean=false;    /*for Econsult*/
-  pathName:any;
-  clinicList=[];
-  searchInput:any;
-  message:any;
-  filterField = {
+  public clinicList = [];
+  public hideEconsult: Boolean = false   /*HIDE ECONSULT* AT*/
+  public whenDashboardClicked: Boolean = true;  /*for Dashboard*/
+  public whenCreditClicked: Boolean = false;      /*for Credit*/
+  public whenAppoinmentClicked: Boolean = false;  /*for Appointment*/
+  public whenClinicsClicked: Boolean = false;     /*for Clinic*/
+  public whenEConsultClicked: Boolean = false;    /*for Econsult*/
+  public pathName: any;
+  public doctorProfileId: any;
+  public prescriptionId: any;
+  public searchInput: any;
+  public message: any;
+  public filterField = {
     clinicName: ''
   };
   public elementRef;
-  constructor(myElement: ElementRef,private clinicService:ClinicService,private router: Router, private ePrescriptionService: DoctorPrescriptionService,private sharedService: SharedService,private route: ActivatedRoute) {
- 
+  constructor(myElement: ElementRef, private clinicService: ClinicService, private router: Router, private ePrescriptionService: DoctorPrescriptionService, private sharedService: SharedService, private route: ActivatedRoute) {
+
     this.doctorProfileId = localStorage.getItem('doctorId');
 
     this.elementRef = myElement;
 
     // change colour of item depending on route parameter
-    this.sharedService.urlPathInfo.subscribe(data=>{
-   if(data=='/dashboard/main' || data=='/dashboard/profile' || data=='/dashboard/account'){
+    this.sharedService.urlPathInfo.subscribe(data => {
+      if (data == '/dashboard/main' || data == '/dashboard/profile' || data == '/dashboard/account') {
+        this.hideEconsult=false;
         this.dashboardChangeColor();
       }
-      else if(data=='/dashboard/clinic'){
-       this.clinicsColourChange();
+      else if (data == '/dashboard/clinic') {
+        this.hideEconsult=false;
+        this.clinicsColourChange();
       }
-      else if(data=='/dashboard/appointment'){
+      else if (data == '/dashboard/appointment') {
+        this.hideEconsult=false;
         this.appoinmentColourChange();
       }
-      else if(data=='/dashboard/consultnew/diagnosis' || data=='/dashboard/consultnew/diagnosisform' || data=='/dashboard/consultnew/suggest' || data=='/dashboard/consultnew/medicare' || data=='/dashboard/consultnew/instruction' || data=='/dashboard/consultnew/lifestyle' || data=='/dashboard/consultnew/followup' ){
+      else if (data == '/dashboard/consultnew/diagnosis' || data == '/dashboard/consultnew/diagnosisform' || data == '/dashboard/consultnew/suggest' || data == '/dashboard/consultnew/medicare' || data == '/dashboard/consultnew/instruction' || data == '/dashboard/consultnew/lifestyle' || data == '/dashboard/consultnew/followup') {
+        this.hideEconsult=true;
         this.eConsultColourChange();
       }
-      else if(data=='/dashboard/raisebill' || data=='/dashboard/credit'){
+      else if (data == '/dashboard/consultnew/diagnosis/:individualId/:appointmentId') {
+        // console.log('dataaa',data)
+        console.log('dataaaghvghvggvhgbhghghghgbhuftfyhjkml,jkhghj')
+
+        this.hideEconsult=true;
+        // console.log('hideconsult',this.hideEconsult)
+        this.eConsultColourChange();
+      }
+      else if (data == '/dashboard/raisebill' || data == '/dashboard/credit') {
+        this.hideEconsult=false;
         this.creditColourChange();
       }
-      else if(data=='/dashboard/appnew'){
+      else if (data == '/dashboard/appnew') {
+        this.hideEconsult=false; 
         this.appoinmentColourChange();
       }
     })
@@ -60,7 +75,7 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit() {
   }
-// create prescription
+  // create prescription
   // navigateEprescription() {
   //   let prescriptionData = {
   //     adviceId: [],
@@ -84,100 +99,105 @@ export class NavbarComponent implements OnInit {
   //     });
   // }
   //using ngClass on click for dashboard
-  dashboardChangeColor(){  
-    this.whenDashboardClicked=true;
-    this.whenCreditClicked=false;
-  this.whenEConsultClicked=false;
-  this.whenClinicsClicked=false;
-  this.whenAppoinmentClicked=false;
-}
+  dashboardChangeColor() {
+    this.whenDashboardClicked = true;
+    this.whenCreditClicked = false;
+    this.whenEConsultClicked = false;
+    this.whenClinicsClicked = false;
+    this.whenAppoinmentClicked = false;
+    this.hideEconsult=false;
+  }
   //using ngClass on click for EConsult
-eConsultColourChange(){
-  this.whenEConsultClicked=true;  
-  this.whenCreditClicked=false; 
-  this.whenDashboardClicked=false;
-  this.whenAppoinmentClicked=false;
-  this.whenClinicsClicked=false;
-}
- //using ngClass on click for Clinics
- clinicsColourChange(){
-  this.whenClinicsClicked=true; 
-  this.whenDashboardClicked=false;
-  this.whenEConsultClicked=false;
-  this.whenAppoinmentClicked=false;
-  this.whenCreditClicked=false; 
-}
- //using ngClass on click for Appoinment
- appoinmentColourChange(){
-  this.whenAppoinmentClicked=true; 
-  this.whenDashboardClicked=false;
-  this.whenEConsultClicked=false;
-  this.whenCreditClicked=false;
-  this.whenClinicsClicked=false;
-}
- //using ngClass on click for Credit
- creditColourChange(){
-  this.whenCreditClicked=false; 
-  this.whenDashboardClicked=false;
-  this.whenEConsultClicked=false;
-  this.whenAppoinmentClicked=false;
-  this.whenClinicsClicked=false;
-}
+  eConsultColourChange() {
+    this.whenEConsultClicked = true;
+    this.whenCreditClicked = false;
+    this.whenDashboardClicked = false;
+    this.whenAppoinmentClicked = false;
+    this.whenClinicsClicked = false;
+    this.hideEconsult=true;
+  }
+  //using ngClass on click for Clinics
+  clinicsColourChange() {
+    this.whenClinicsClicked = true;
+    this.whenDashboardClicked = false;
+    this.whenEConsultClicked = false;
+    this.whenAppoinmentClicked = false;
+    this.whenCreditClicked = false;
+    this.hideEconsult=false;
+  }
+  //using ngClass on click for Appoinment
+  appoinmentColourChange() {
+    this.whenAppoinmentClicked = true;
+    this.whenDashboardClicked = false;
+    this.whenEConsultClicked = false;
+    this.whenCreditClicked = false;
+    this.whenClinicsClicked = false;
+    this.hideEconsult=false;
+  }
+  //using ngClass on click for Credit
+  creditColourChange() {
+    this.whenCreditClicked = false;
+    this.whenDashboardClicked = false;
+    this.whenEConsultClicked = false;
+    this.whenAppoinmentClicked = false;
+    this.whenClinicsClicked = false;
+    this.hideEconsult=false;
+  }
 
-// search input
-searchResult(value){
-  this.clinicList=[];
-  this.filterField.clinicName = value;
-if(value){
-    this.clinicService.getCliniclist(localStorage.getItem('doctorId')).subscribe(data => {
-      let result:any={}
-      result=data
-      console.log('clinic list search',result)
-      if(result.result.length!=0){
-        this.clinicList = result.result;
-        this.searchInput="";
-        console.log(this.clinicList);
+  // search input
+  searchResult(value) {
+    this.clinicList = [];
+    this.filterField.clinicName = value;
+    if (value) {
+      this.clinicService.getCliniclist(localStorage.getItem('doctorId')).subscribe(data => {
+        let result: any = {}
+        result = data
+        console.log('clinic list search', result)
+        if (result.result.length != 0) {
+          this.clinicList = result.result;
+          this.searchInput = "";
+          console.log(this.clinicList);
+        }
+        // else{
+        //   this.message='No clinic found'
+        // }
+      },
+        err => {
+        })
+    }
+  }
+
+  navigateToClinic() {
+    this.clinicList = [];
+    // this.searchInput=" ";
+    this.router.navigate(['dashboard/clinic']);
+  }
+
+  //toggle sidenav in mobileview
+  toggleopen() {
+    this.sharedService.togglesidenav(true);
+  }
+
+  // hide search results on clicking outside
+  handleClick(event) {
+    var clickedComponent = event.target;
+    var inside = false;
+    do {
+      if (clickedComponent === this.elementRef.nativeElement) {
+        inside = true;
       }
-    // else{
-    //   this.message='No clinic found'
-    // }
-    },
-      err => {
-      })
-  }
-}
-
-navigateToClinic(){
-  this.clinicList=[];
-  // this.searchInput=" ";
-  this.router.navigate(['dashboard/clinic']);
-}
-
-//toggle sidenav in mobileview
-toggleopen(){
-  this.sharedService.togglesidenav(true);
-}
-
-// hide search results on clicking outside
-handleClick(event) {
-  var clickedComponent = event.target;
-  var inside = false;
-  do {
-    if (clickedComponent === this.elementRef.nativeElement) {
-      inside = true;
-    }
-    clickedComponent = clickedComponent.parentNode;
-  } while (clickedComponent);
-  if (inside) {
-    console.log('inside');
-  } else {
-    console.log('outside');
-    if (this.clinicList.length!=0) {
-      this.clinicList =[];
+      clickedComponent = clickedComponent.parentNode;
+    } while (clickedComponent);
+    if (inside) {
+      console.log('inside');
+    } else {
+      console.log('outside');
+      if (this.clinicList.length != 0) {
+        this.clinicList = [];
+      }
     }
   }
 }
-}
 
 
-  
+
